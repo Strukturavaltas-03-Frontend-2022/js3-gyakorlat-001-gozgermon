e134377d01e85d0bef3382043500d8b26646f6c4
Mónika <gergye.monika@gmail.com>
2022-09-04 17:53:21 +0200

    első gyakorlás

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,14 @@
+function removeDuplicatesFromArray(arra_d) {
+    let uniqueEntry = [];
+    arra_d.forEach((c) => {
+        if (!uniqueEntry.includes(c)) {
+            uniqueEntry.push(c);
+        }
+    })
+
+    return uniqueEntry
+
+}
+
+
 export default removeDuplicatesFromArray;
